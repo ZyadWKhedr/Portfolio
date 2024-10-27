c3b11aaf8494bfe24d83a0a3046b10d6516607cb
Zyad_Senpai <153391378+ZyadWKhedr@users.noreply.github.com>
2024-10-27 10:44:54 +0300

    Update send_messages.php

--- send_messages.php
+++ send_messages.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $to = 'your-email@example.com'; // Replace with your email address
+    $to = 'ziad.w.khedr@gmail.com'; 
     $subject = 'New message from portfolio contact form';
     $headers = "From: $email" . "\r\n" .
                "Reply-To: $email" . "\r\n" .
